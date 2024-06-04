e0fb6a014317949426264b23515d03e69083d4d4
vlad Amirov <vladamirov@list.ru>
2024-06-04 23:13:43 +0300

    main rework

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ class FileAlgous:
 
     def scan_directory(self) -> list[str]:
         """Cканирует директорию и возвращает путь до всех объектов от папки входа список всех объектов .jpg"""
-
-        files_in_dirs = glob.glob(self.path + '/**/*.jpg', recursive=True)
+        files_in_dirs = sorted(glob.glob(self.path + '/**/*.jpg', recursive=True))
         # directory_path/     the dir
         # **/       every file and dir under my_path
         # *.txt     every file that ends with '.jpg'
@@ -20,8 +19,8 @@ class FileAlgous:
         return files_in_dirs
 
     @staticmethod
-    def split_path_name_to_sequence_and_padding(path_name) -> tuple[str, str]:
-        """Разделяем путь до файла на название секвенции и номер элемента секвенции"""
+    def split_path_name_to_sequence_and_padding(path_name: object) -> tuple[str, str, str]:
+        """Разделяем путь до файла на название секвенции и номер элемента секвенции и разделитель"""
         # разделяем путь на атомарные элементы
         parts = os.path.normpath(path_name).split(os.sep)
         # разделяем наименование секвенции и последний элемент секвенции
@@ -30,31 +29,52 @@ class FileAlgous:
                                                                      string=name_sequence_with_padding,
                                                                      maxsplit=1)
         name_sequence, padding_sequence = padding_sequence_reversed[::-1], name_sequence_reversed[::-1]
+        # получаем разделитель между наименованием секвенции и номером кадра
+        delimiter = name_sequence_with_padding.lstrip(padding_sequence).rstrip(name_sequence)
+        # костыльная проверка, но схема рабочая
+        if delimiter == '':
+            if name_sequence.find("_"):
+                delimiter = '_'
+            else:
+                delimiter = ' '
 
-        return name_sequence, padding_sequence
+        return name_sequence, padding_sequence, delimiter
 
     def choice_sequence(self) -> dict[str, list[str]]:
         """Функция определяющая к какой секвенции отнести изображение"""
         dict_of_sequence = {}
 
         for file in self.files:
-            # разделяем путь до файла на название секвенции и номер элемента секвенции
-            name_sequence, padding_sequence = self.split_path_name_to_sequence_and_padding(file)
-            # добавляем к названию секвенции длину номера кадра
-            name_sequence_length = name_sequence + str(len(padding_sequence))
-            # если секвенция с именем name_sequence_length уже была, значение по ключу name_sequence - существующий
-            # список, если секвенции с таким именем не было берем дефолтное значение []
-            list_files_sequence = dict_of_sequence.get(name_sequence_length, [])
-            # добавляем в список по значению ключа путь до файла
-            dict_of_sequence[name_sequence_length] = list_files_sequence + [name_sequence, padding_sequence]
+            # отделяем от пути до файла папку в которой лежат все папки с секвенциями
+            new_file = file.strip(self.path)
+            # определяем папку в которой лежит секвенция
+            head = os.path.split(new_file)[0]
+            # если секвенция с именем head уже была, значение словаря по ключу head - существующий список,
+            # если секвенции с таким именем не было берем дефолтное значение []
+            list_files_sequence = dict_of_sequence.get(head, [])
+            # добавляем в список по значению ключа абсолютный путь до файла
+            dict_of_sequence[head] = list_files_sequence + [file]
+        return dict_of_sequence
 
-        for name_seq, list_path in dict_of_sequence.items():
+    def make_mov_file(self):
+        dict_of_sequence = self.choice_sequence()
+        for path_name_seq, list_path in dict_of_sequence.items():
+            name_sequence, padding_sequence, delimiter = self.split_path_name_to_sequence_and_padding(list_path[0])
+            # print((name_sequence, padding_sequence, delimiter))
             os.system(
-                f"""ffmpeg -framerate 24 -start_number {int(list_path[1])} -i '{name_seq[0:-1]}_%0{len(list_path[1])}d.jpg' {name_seq}.mov""")
+                f"""ffmpeg -framerate 24 -start_number {int(padding_sequence)} -i '{self.path}\\{path_name_seq}\\
+                    {name_sequence}{delimiter}%0{len(padding_sequence)}d.jpg' '{name_sequence}.mov' """)
+            # тут с путями уже путаюсь, не понимаю в каком формате передать атрибуту -input файл - с абсолютным путём?
+            # или локальным от папки входа? или непосредственно сам файл, а он сам найдет?(((((не найдет)))))
+
+            # os.system( f"""ffmpeg -framerate 24 -start_number {int(padding_sequence)} -i '{name_sequence}{delimiter}
+            # %0{len(padding_sequence)}d.jpg' '{name_sequence}.mov' """)
+
+            # os.system(f"""ffmpeg -framerate 24 -start_number {int(padding_sequence)} -i '{path_name_seq}
+            # \\{name_sequence}{delimiter}%0{len(padding_sequence)}d.jpg' '{name_sequence}.mov' """)
 
 
 if __name__ == '__main__':
-
     load_dotenv()
-    FileAlgous(os.environ.get('PATH_TO_IMAGES')).choice_sequence()
-
+    FileAlgous(os.environ.get('PATH_TO_IMAGES')).make_mov_file()
+# print(FileAlgous(os.environ.get('PATH_TO_IMAGES')).choice_sequence())
